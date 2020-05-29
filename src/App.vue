ac589d861c64b1b21235b9d2d0c07728f7e46091
DevonTheFloor <devonthefloor@free.fr>
2020-05-29 07:40:22 +0200

    page login befor axios

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Accueil/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Accueil from './components/Accueil'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Accueil
   }
 }
 </script>
