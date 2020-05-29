2377114ff1016e0a3e73035ee336824989bae88e
DevonTheFloor <devonthefloor@free.fr>
2020-05-29 10:29:33 +0200

    add front file

---
+++ front/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Accueil/>
+  </div>
+</template>
+
+<script>
+
+import Accueil from './components/Accueil'
+
+export default {
+  name: 'App',
+  components: {
+    Accueil
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ front/src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer>
+    <div class="col-lg-12">
+        <ol>
+        </ol>
+      </div>
+      <div class="col-lg-12">
+      <ol>
+      </ol>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style>
+footer {
+  padding: 2%;
+  bottom: 0px;
+}
+</style>
